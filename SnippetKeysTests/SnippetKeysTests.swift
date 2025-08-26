74c235cdbeb717b332b3c50e98bb5059cd69efd2
Mark Stramaglia <m@bludgeonsoft.org>
2025-08-25 17:26:03 -0700

    Initial Commit

---
+++ SnippetKeysTests/SnippetKeysTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnippetKeysTests.swift
+//  SnippetKeysTests
+//
+//  Created by Mark Stramaglia on 8/25/25.
+//
+
+import Testing
+@testable import SnippetKeys
+
+struct SnippetKeysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
